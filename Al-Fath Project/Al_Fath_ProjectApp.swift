79129be2fca877ed475a6b5c7d64abb1233072cc
Muhammad Firdaus <mfirdaus1453@gmail.com>
2021-10-07 15:03:03 +0800

    Initial Commit

---
+++ Al-Fath Project/Al_Fath_ProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  Al_Fath_ProjectApp.swift
+//  Al-Fath Project
+//
+//  Created by Muhammad Firdaus on 07/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Al_Fath_ProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
