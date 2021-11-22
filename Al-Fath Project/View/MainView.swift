06f97ae32a6a437f26e8db7182d1689343a3a10e
riosudarsono <mas.riosudarsono@gmail.com>
2021-11-22 17:47:49 +0700

    Custom Tabview

--- Al-Fath Project/View/MainView.swift
+++ Al-Fath Project/View/MainView.swift
@@ -22,40 +22,61 @@ struct MainView: View {
     
     var body: some View {
         ZStack {
-            HStack {
+            ZStack(alignment: .bottom) {
+                if selectedTab == 1 {
+                    LearnView(viewModel: learnVM)
+                } else if selectedTab == 2{
+                    DictionaryListView(vm: dictionaryVM)
+                } else if selectedTab == 3 {
+                    SettingsView()
+                }
+                
+                HStack{
+                    Spacer()
+                    if selectedTab == 1 { Image("ic_learn_").onTapGesture {
+                        selectedTab = 1
+                    } }
+                    else { Image("ic_learn").onTapGesture {
+                        selectedTab = 1
+                    } }
+                    
+                    Spacer()
+                    if selectedTab == 2 { Image("ic_dictionary_").onTapGesture {
+                        selectedTab = 2
+                    } }
+                    else { Image("ic_dictionary").onTapGesture {
+                        selectedTab = 2
+                    } }
+                    
+                    Spacer()
+                    if selectedTab == 3 { Image("ic_settings_").onTapGesture {
+                        selectedTab = 3
+                    } }
+                    else { Image("ic_settings").onTapGesture {
+                        selectedTab = 3
+                    } }
+                    Spacer()
+                }
+                .padding(.top, 16)
+                .padding(.bottom, 24)
+                .background(Color.primary)
+                .cornerRadius(16)
+                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 5, x: 2, y: 2)
             }
-            .frame(height: 40)
-            .cornerRadius(15)
             .background(Color.secondary)
+            .cornerRadius(8)
+            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 5, x: 2, y: 2)
             
-//            NavigationLink(destination: Learn1View(viewModel: learnVM), isActive: $learnVM.learn1Show) {
-//                Text("").hidden()
-//            }
-            
-            NavigationLink(destination: DictionaryView(vm: dictionaryVM,
-                                                       player: player),
-                           isActive: $dictionaryVM.showDictionary) {
+            NavigationLink(destination: DictionaryView(vm: dictionaryVM, player: player),isActive: $dictionaryVM.showDictionary) {
             }
-            
-            TabView(selection: $selectedTab) {
-                LearnView(viewModel: learnVM).tabItem {
-                    if selectedTab == 1 { Image("ic_learn_") }
-                    else { Image("ic_learn") }
-                }.tag(1)
-                DictionaryListView(vm: dictionaryVM).tabItem {
-                    if selectedTab == 2 { Image("ic_dictionary_") }
-                    else { Image("ic_dictionary") }
-                }.tag(2)
-                SettingsView().tabItem {
-                    if selectedTab == 3 { Image("ic_settings_") }
-                    else { Image("ic_settings") }
-                }.tag(3)
-            }
-            .navigationBarHidden(true)
             
         }
+//        .background(Color.black.opacity(0.8))
         .background(Color.secondary)
+        .cornerRadius(8)
+        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 5, x: 2, y: 2)
         .navigationBarHidden(true)
+        .edgesIgnoringSafeArea(.vertical)
     }
     
     
